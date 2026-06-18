a8713d5dc042d3e6966f1df06d5911d7ffc0ff53
“niharikaaitham” <niharika.aitham@gmail.com>
2026-06-18 10:46:36 +0100

    Theory Added

---
+++ src/abstractClass/Theory.java
@@ -0,0 +1,11 @@
+package abstractClass;
+
+public class Theory {
+    /*
+    A class declared with "abstract" keyword is called abstract class. An Abstract class is nothing but an incomplete class where programmers can declare complete as well as incomplete methods in it. Programmers can declare incomplete methods as abstract methods, by declaring a keyword called "abstract" in front of the method.
+We can't create objects of abstract class, to create objects of abstract class we need to make use of concrete class.
+Concrete class:
+A class which provides definitions for all the incomplete methods which are present in abstract class with the help of extends keywords is called concrete class.
+
+     */
+}
