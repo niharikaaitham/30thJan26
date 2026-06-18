925c1fc7fc88acfb90f92cc04004375f35a72738
“niharikaaitham” <niharika.aitham@gmail.com>
2026-06-18 11:04:27 +0100

    Added notes

--- src/abstractClass/Theory.java
+++ src/abstractClass/Theory.java
@@ -1,11 +1,25 @@
-package abstractClass;
+    package abstractClass;
 
-public class Theory {
-    /*
-    A class declared with "abstract" keyword is called abstract class. An Abstract class is nothing but an incomplete class where programmers can declare complete as well as incomplete methods in it. Programmers can declare incomplete methods as abstract methods, by declaring a keyword called "abstract" in front of the method.
-We can't create objects of abstract class, to create objects of abstract class we need to make use of concrete class.
-Concrete class:
-A class which provides definitions for all the incomplete methods which are present in abstract class with the help of extends keywords is called concrete class.
+    public class Theory {
+        /*
+        A class declared with "abstract" keyword is called abstract class. An Abstract class is nothing but an incomplete
+        class where programmers can declare complete as well as incomplete methods in it. Programmers can declare
+        incomplete methods as abstract methods, by declaring a keyword called "abstract" in front of the method.
 
-     */
-}
+        We can't create objects of abstract class, to create objects of abstract class we need to make use of concrete
+        class.
+        Concrete class:
+        A class which provides definitions for all the incomplete methods which are present in abstract class
+        with the help of extends keywords is called concrete class.
+
+        Note→
+        If a concrete class inherits from an abstract class but fails to implement all the abstract methods, that concrete
+        class must also be declared as an abstract class.
+        To create a usable, non-abstract (concrete) class, a new class must be created to fully implement the
+        remaining abstract methods.
+
+        A single abstract class can be extended by multiple concrete classes, which is an example of the
+        Hierarchical Inheritance concept.
+
+         */
+    }
